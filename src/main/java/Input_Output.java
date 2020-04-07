8a712b15f4a468e2e4159dd1d4b0c95d04e61d5b
Christine <chriztine.cyrus@gmail.com>
2020-04-08 04:19:08 +1000

    new java class Input_Output.java by christine

--- src/main/java/Input_Output.java
+++ src/main/java/Input_Output.java
@@ -1,2 +1,21 @@
+import java.io.File;
+import java.util.Scanner;
+
+// ask user to input file name or use JFileChooser (take from Oracle)
 public class Input_Output {
+
+    public void inputFile(){
+        Scanner readInput = new Scanner (System.in); // creates readInput scanner
+        System.out.println("Enter file name");
+
+        String fileName = readInput.nextLine(); // reads next line aka the user input
+
+        File inputFile = new File (fileName); // opens fileName, aka file has to be in the same folder.
+
+    }
+
+
+
+
+
 }
