422d1e1f5399f204daba2db13054c45799b85ffd
Wan Jack Lee <charliewanj@outlook.com>
2020-04-08 12:14:15 +1000

    implementation preprocessing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     // provide the connection of UI with the I/O
     // performing read and write file
+
+    static String fileInputPath ;
+    static String fileOutputPath;
+
+    public static void main(String[] args) {
+        // manually scan the filename
+        Scanner readInput = new Scanner (System.in); // creates readInput scanner
+        System.out.println("Enter file name");
+        //
+        fileInputPath = readInput.nextLine(); // reads next line aka the user input
+        System.out.println( "Input file : "+ fileInputPath);
+
+        try{
+            System.out.println(ReadFile.read(fileInputPath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/main/java/ReadFile.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ReadFile {
+    public static String read(String filePath) throws Exception {
+
+        String line;
+        File fname = new File(filePath);
+        try{
+            InputStream fis = new FileInputStream(fname.getAbsolutePath());
+        }
+        catch(FileNotFoundException error){
+            return "ERROR";
+        }
+        InputStream fis = new FileInputStream(fname.getAbsolutePath());
+        InputStreamReader file = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(file);
+
+        while ((line=br.readLine()) != null){
+            StringTokenizer tokenizer = new StringTokenizer(line, " \t,;");
+            String str = tokenizer.nextToken().trim().toLowerCase();
+            System.out.println(str);
+            if (str.equals("ncols")){
+
+            }
+
+        }
+
+//        StringTokenizer tokenizer = new StringTokenizer()
+
+
+        return "";
+    }
+}
